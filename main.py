1aa007184b685709937e34fa7d9551acd2c79bef
nmmramor <nmmramor@gmail.com>
2019-04-30 13:11:25 -0500

    return index.html and templace it correctly

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 import scrape_mars
 import pymongo
 
 app = Flask(__name__)
 
+client = pymongo.MongoClient('mongodb://localhost:27017') 
+mars_collection = client.mars_db.mars_mission
+
 @app.route("/scrape")
 def scrape_data():
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn) 
-    db = client.mars_db
-    
-    mars_collection = db.mars_mission
-
     scraped = scrape_mars.scrape()
     mars_collection.insert_one(scraped)
     
     return scraped
+
+@app.route("/")
+def index():
+    mars = mars_collection.find_one()
+    return render_template("index.html", mars=mars)
+
+
+if __name__ == "__main__":
+    app.run()
